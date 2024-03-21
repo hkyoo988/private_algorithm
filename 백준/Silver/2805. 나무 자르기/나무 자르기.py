0061232359bfb7b874268e4a2d2286f2d53ba695
Heungkuk_201702305 <101104244+hkyoo988@users.noreply.github.com>
2024-03-21 09:09:29 +0900

    [Silver II] Title: 나무 자르기, Time: 2784 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,31 +1,45 @@
 import sys
 
-n, m = tuple(map(int, sys.stdin.readline().split()))
+n, m = tuple(map(int, input().split()))
 
-arr = list(map(int, sys.stdin.readline().split()))
+arr = list(map(int, input().split()))
 
 arr.sort()
 
-left = 0
-right = arr[-1]
-
-while left <= right:
-
-    center = (left+right)//2
-
-    wood = 0
+def calc_tree(start):
+    result = 0
 
     for i in range(n):
-        if arr[i] > center:
-            wood += arr[i] - center
+        num = arr[i] - start
+        if num > 0:
+            result += num
+    return result
 
-    
-    if wood < m:
-        right = center-1
-    elif wood > m:
-        left = center+1
-    else:
-        right = center
-        break
+ans, get_tree = 0, 0
+def cut_tree(bottom, top):
+    global ans, get_tree
 
-print(right)
+    while bottom <= top:
+        # if get_tree < m:
+        #     mid = (bottom + mid-1) // 2
+        mid = (bottom + top) // 2
+
+        get_tree = calc_tree(mid)
+
+        if get_tree > m:
+            ans = mid
+            bottom = mid + 1
+        elif get_tree == m:
+            ans = mid
+            break
+        else:
+            top = mid - 1
+        
+        # if bottom == top:
+        #     h = calc_tree(bottom)
+        #     ans = bottom
+        #     break
+
+cut_tree(0, arr[-1])
+
+print(ans)
