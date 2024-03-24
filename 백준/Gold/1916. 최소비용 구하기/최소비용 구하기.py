ade24ccc3b143d80fc66d12a3b90a4c928a6e521
Heungkuk_201702305 <101104244+hkyoo988@users.noreply.github.com>
2024-03-24 22:44:35 +0900

    [Gold V] Title: 최소비용 구하기, Time: 356 ms, Memory: 57532 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+import heapq
+
+n = int(sys.stdin.readline())
+
+m = int(sys.stdin.readline())
+
+arr = [[] for _ in range(n+1)]
+
+for _ in range(m):  
+    s, e, c = tuple(map(int, sys.stdin.readline().split()))
+
+    arr[s].append((e, c))
+
+
+s, e = tuple(map(int, sys.stdin.readline().split()))
+
+pq = []
+
+heapq.heappush(pq, (0, s))
+cost = [sys.maxsize]*(n+1)
+cost[s] = 0
+while pq:
+    c, s = heapq.heappop(pq)
+
+    if c > cost[s]:
+        continue
+    for ns, nc in arr[s]:
+        c1 = c + nc
+        if c1 < cost[ns]:
+            cost[ns] = c1
+            heapq.heappush(pq, (c1, ns))
+
+print(cost[e])
