fd7fece5508e4358f1d23708616f495330ef65c3
Heungkuk_201702305 <101104244+hkyoo988@users.noreply.github.com>
2024-03-27 14:10:51 +0900

    [Platinum V] Title: 임계경로, Time: 244 ms, Memory: 43568 KB -BaekjoonHub

---
+++ 백준/Platinum/1948. 임계경로/임계경로.py
@@ -0,0 +1,47 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline().rstrip())
+
+m = int(sys.stdin.readline().rstrip())
+
+arr = [[] for _ in range(n+1)]
+
+e = [0]*(n+1)
+for _ in range(m):
+    start, end, t = tuple(map(int, sys.stdin.readline().split()))
+
+    arr[start].append((end, t))
+    e[end] += 1
+
+START, END = tuple(map(int, sys.stdin.readline().split()))
+
+q = deque()
+q.append((START))
+time, street = [0]*(n+1), [[] for _ in range(n+1)]
+while q:
+    x = q.popleft()
+
+    for i in arr[x]:
+        end, t = i
+        e[end] -= 1
+        if time[end] < time[x] + t:
+            time[end] = time[x] + t
+            street[end] = [x]
+        elif time[end] == time[x] + t:
+            street[end].append(x)
+        if e[end] == 0:
+            q.append((end))
+
+q = deque([END])
+route = set()
+while q:
+    x = q.popleft()
+
+    for i in street[x]:
+        if (x, i) not in route:
+            route.add((x, i))
+            q.append(i)
+
+print(time[end])
+print(len(route))
