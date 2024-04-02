5b848131e141a7be05c0a2984a2e0a6009256dbb
Heungkuk_201702305 <101104244+hkyoo988@users.noreply.github.com>
2024-04-02 16:55:40 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -1,17 +1,18 @@
 import sys
 
+dp = [0]*(10000+1)
+dp[0] = 1
+dp[1] = 1
+for j in [1, 2, 3]:
+    for k in range(2, 10000+1):
+        if k-j >= 0:
+            dp[k] += dp[k-j]
+
 t = int(sys.stdin.readline().rstrip())
 for i in range(t):
     n = int(sys.stdin.readline().rstrip())
     if n == 2:
         print(2)
         continue
-    dp = [0]*(n+1)
-    dp[0] = 1
-    dp[1] = 1
-    for j in [1, 2, 3]:
-        for k in range(2, n+1):
-            if k-j >= 0:
-                dp[k] += dp[k-j]
 
-    print(dp[n] if n != 0 else 0)
+    print(dp[n])
