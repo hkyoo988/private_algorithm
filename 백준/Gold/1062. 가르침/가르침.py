eaa196cd834b20e38790a1cbdbecc99efd979893
hkyoo988 <101104244+hkyoo988@users.noreply.github.com>
2024-08-05 17:47:51 +0900

    [Gold IV] Title: 가르침, Time: 180 ms, Memory: 110460 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.py
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -1,30 +1,32 @@
 from itertools import combinations
 
+def wordTobit(word):
+    bit = 0
+    for char in word:
+        bit = bit | (1 << ord(char) - ord('a'))
+
+    return bit
+
 # 입력 받기
 N, K = map(int, input().split())
 
-texts = []
-for _ in range(N):
-    # 문자열에서 'anta', 'tica'를 제거한 후 중복 제거
-    text = set(input().strip()[4:-4])
-    texts.append(text)
-
-# 기본적으로 배우고 있어야 하는 알파벳 (a, c, i, n, t)
-base_learned = {0, 2, 8, 13, 19}
-unlearned = [i for i in range(26) if i not in base_learned]
+words = [input().rstrip() for _ in range(N)]
+bits = list(map(wordTobit, words))
+base_bit = wordTobit('antic')
 
 # K가 5보다 작으면 a, c, i, n, t를 배우지 못하므로 읽을 수 있는 단어가 없음
 if K < 5:
     print(0)
 else:
+
+    unlearned = [1 << i for i in range(26) if not (base_bit & 1 << i)]
     max_readable = 0
     # 조합 생성 및 최대 읽을 수 있는 단어 수 계산
     for combo in combinations(unlearned, K - 5):
-        learned = set(combo) | base_learned
+        know_bit = sum(combo) | base_bit
         readable_count = 0
-        for text in texts:
-            if all((ord(char) - ord('a')) in learned for char in text):
+        for bit in bits:
+            if bit & know_bit == bit:
                 readable_count += 1
-        max_readable = max(max_readable, readable_count)
-    
+        max_readable = max(max_readable, readable_count)    
     print(max_readable)
